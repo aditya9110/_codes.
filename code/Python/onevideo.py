1017a9d13646ae31b8a8a6ae2578155505c8c797
8veDant <71282147+8veDant@users.noreply.github.com>
2020-09-29 15:42:23 +0530

    Add files via upload

---
+++ code/Python/onevideo.py
@@ -0,0 +1,21 @@
+print("Hello World")
+
+# This is a comment
+'''this is a comment'''
+abc ="hello" 
+#string can be declared by any name not by the no. can use under scroll to declare
+print(abc)
+
+
+#data types is python are 
+''' numbers, strings, tuples, dictionaries,  '''
+#arithmetic operators
+print("The value of 3+5 ", 3+5)
+print("The value of 3-5 ", 3-5)
+print("The value of 3*5 ", 3*5)
+print("The value of 3/5 ", 3/5)
+print("The value of 3**5 ", 3**5)
+print("The value of 3//5 ", 3//5)
+
+#to print " 
+print("This is a \"")
